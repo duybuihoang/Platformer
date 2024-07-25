4ca5ebf8bee5d1b8c9be736b5c2080b62b98571e
duybuihoang <91789217+duybuihoang@users.noreply.github.com>
2024-07-26 02:46:03 +0700

    feat: add jump animation, fix infinite jump

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -6,26 +6,75 @@ public class PlayerController : MonoBehaviour
 {
     private float movementInputDirection;
 
-    private bool isFacingRight = true; 
+    private int amoutOfJumpsLeft;
+
+    private bool isFacingRight = true;
+    private bool isWalking;
+    public bool isGrounded;
+    private bool canJump;
 
     private Rigidbody2D rb;
+    private Animator anim;
+
+    public int amountOfJumps = 1;
 
     public float movementSpeed = 10f;
     public float jumpForce = 16.0f;
+    public float groundCheckRadius;
+
+    public Transform groundCheck;
+
+    public LayerMask whatIsGround;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        amoutOfJumpsLeft = amountOfJumps;
     }
 
     void Update()
     {
         CheckInput();
         CheckMovementDirection();
+        UpdateAnimation();        
+        CheckIfCanJump();
+
     }
 
     private void FixedUpdate()
     {
         ApplyMovement();
+        CheckSurroundings();        
+
+    }
+
+    private void UpdateAnimation()
+    {
+        anim.SetBool("isWalking", isWalking);
+        anim.SetBool("isGrounded", isGrounded);
+        anim.SetFloat("yVelocity", rb.velocity.y);
+    }
+
+    private void CheckIfCanJump()
+    {
+        if (isGrounded && rb.velocity.y <= 0.01f)
+        {
+            amoutOfJumpsLeft = amountOfJumps;
+        }
+
+        if(amoutOfJumpsLeft <= 0)
+        {
+            canJump = false;
+        }
+        else
+        {
+            canJump = true;
+        }
+    }
+    private void CheckSurroundings()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
     }
 
     private void CheckMovementDirection()
@@ -37,6 +86,15 @@ public class PlayerController : MonoBehaviour
         else if (!isFacingRight && movementInputDirection > 0)
         {
             Flip();
+        }
+
+        if (rb.velocity.x != 0)
+        {
+            isWalking = true;
+        }
+        else
+        {
+            isWalking = false;
         }
     }
     private void CheckInput()
@@ -51,7 +109,11 @@ public class PlayerController : MonoBehaviour
 
     private void Jump()
     {
-        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        if (canJump)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            amoutOfJumpsLeft--;
+        }
     }
     private void Flip()
     {
@@ -62,5 +124,10 @@ public class PlayerController : MonoBehaviour
     private void ApplyMovement()
     {
         rb.velocity = new Vector2(movementSpeed * movementInputDirection, rb.velocity.y);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
     }
 }
